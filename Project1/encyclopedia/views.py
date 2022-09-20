4fbae2bdd9c8a7c7300a06ef081e2df6213be3ac
Sarah Edwards <astirilane@gmail.com>
2022-09-19 21:51:54 -0400

    Updated form for start of POST
    Updated form for start of POST

--- Project1/encyclopedia/views.py
+++ Project1/encyclopedia/views.py
@@ -1,15 +1,19 @@
+from pickle import NONE
 from django.shortcuts import render
 import markdown2
-# from django.template import RequestContext
+from django.template import RequestContext
 from django import forms
 
 from . import util
 
+
+# Create new entry form class for wiki entries
 class NewEntryForm(forms.Form):
     name = forms.CharField(label="New Entry Title")
     entry_info = forms.CharField(widget=forms.Textarea, label="Wiki Entry Data")
 
 
+# Index display page and search
 def index(request):
     if request.method == "POST":
         search = request.POST['q']
@@ -22,9 +26,11 @@ def index(request):
             "entries": util.list_entries()
         })
 
+
+# Display searched wiki entry
 def entry_view(request, entry):
     if not util.get_entry(entry):
-        return render (request, "encyclopedia/not_found.html")
+        return render(request, "encyclopedia/not_found.html")
     else:
         return render(request, "encyclopedia/entry_data.html", {
             "entry_data": markdown2.markdown(util.get_entry(entry)),
@@ -32,7 +38,19 @@ def entry_view(request, entry):
     })
 
 
+# Add a new wiki entry 
 def new_entry(request):
-    return render(request, "encyclopedia/new_entry.html", {
-        "form": NewEntryForm
-    })
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            entry_info = form.cleaned_data["entry_info"]
+            if util.get_entry(name) != None:
+                util.save_entry(name, entry_info)
+                return render(request, "encyclopedia/not_found.html")
+        else:
+            return render(request, "encyclopedia/not_found.html")
+    else:
+        return render(request, "encyclopedia/new_entry.html", {
+            "form": NewEntryForm
+        })
