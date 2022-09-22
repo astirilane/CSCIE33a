6fb719783415e50341e1fe09f9536aa4ad7e06b9
Sarah Edwards <astirilane@gmail.com>
2022-09-21 21:31:11 -0400

    Create random page
    Added page to generate a random entry to view from the list of entries in the wiki

--- Project1/encyclopedia/urls.py
+++ Project1/encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<str:entry>", views.entry_view, name="entry_view"),
     path("wiki/<str:entry>", views.entry_view, name="entry_view"),
-    path("new/", views.new_entry, name="new_entry")
+    path("new/", views.new_entry, name="new_entry"),
+    path("random/", views.random, name="random")
 ]
